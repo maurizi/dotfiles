557a3523ba5c7e12573e51fac4e586279f0e1015
Michael Maurizi <michael.maurizi@gmail.com>
2013-11-14 15:46:57 -0800

    Add (mostly complete) windows vim installation script

---
+++ scripts/vim.cmd
@@ -0,0 +1,21 @@
+chdir /D %~dp0
+
+@if not exist "%HOME%" @set HOME=%HOMEDRIVE%%HOMEPATH%
+@if not exist "%HOME%" @set HOME=%USERPROFILE%
+
+FOR /F "delims=" %%i IN ('git rev-parse --show-toplevel') DO set DOTFILES=%%i
+
+set BASE_DIR=%DOTFILES%/vim
+
+call mklink "%HOME%/_vimrc" "%BASE_DIR%/vimrc"
+call mklink /J "%HOME%/vimfiles" "%BASE_DIR%"
+
+IF NOT EXIST "%BASE_DIR%/vim/bundle" (
+        call mkdir "%BASE_DIR%/vim/bundle"
+)
+
+IF NOT EXIST "%HOME%/vim/bundle/neobundle.vim" (
+    call git clone git://github.com/Shougo/neobundle.vim.git "%BASE_DIR%/bundle/neobundle.vim"
+)
+
+PAUSE
