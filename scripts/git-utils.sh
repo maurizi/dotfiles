1e0dc5ca0f5fbb1e49de2a8984198eeb8ca767a3
Michael Maurizi <mmaurizi@azavea.com>
2014-03-25 15:33:12 -0400

    Minor changes

--- scripts/git-utils.sh
+++ scripts/git-utils.sh
@@ -39,3 +39,4 @@ git config --global alias.ls 'log --pretty=format:"%C(yellow)%h%Cred%d\ %Creset%
 git config --global alias.ll 'log --pretty=format:"%C(yellow)%h%Cred%d\ %Creset%s%Cblue\ [%cn]" --decorate --numstat'
 
 git config --global core.editor vim
+git config --global push.default simple
