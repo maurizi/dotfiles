0c42718180e673cced7978f07822c7c6475c0176
Michael Maurizi <michael.maurizi@gmail.com>
2014-08-03 14:46:16 -0400

    Add initial setup script for babun

---
+++ scripts/babun.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+DOTFILES=$(git rev-parse --show-toplevel);
+
+pact install stow
+
+stow -v -t ~ unix
+
+# TODO: How to install better grep on Windows?
+# sudo apt-get install -qqy silversearcher-ag
+
+echo "Installing hub"
+pact install ruby
+gem install -qy hub >/dev/null
+rm -rf $DOTFILES/bin/hub
+hub hub standalone > $DOTFILES/unix/bin/hub && chmod +x $DOTFILES/unix/bin/hub
+
+echo "Installing git-submodule-move"
+hub clone --quiet iam-TJ/git-submodule-move $DOTFILES/unix/bin/git-submodule-move
+
+echo "Installing git-hooks"
+hub clone icefox/git-hooks $DOTFILES/unix/bin/git-hooks
+
+echo "Setting git aliases"
+git config --global alias.root '!pwd'
+git config --global alias.prune-all '!git remote | xargs -n 1 git remote prune'
+git config --global alias.tree 'log --graph --decorate --pretty=oneline --abbrev-commit'
+git config --global alias.ls 'log --pretty=format:"%C(yellow)%h%Cred%d\ %Creset%s%Cblue\ [%cn]" --decorate'
+git config --global alias.ll 'log --pretty=format:"%C(yellow)%h%Cred%d\ %Creset%s%Cblue\ [%cn]" --decorate --numstat'
+
+git config --global core.editor vim
+git config --global push.default matching
+
+if [ ! -e ~/.vim/bundle/neobundle.vim ]
+then
+    echo "Cloning NeoBundle"
+    git clone --quiet git://github.com/Shougo/neobundle.vim.git ~/.vim/bundle/neobundle.vim
+fi
+vim +NeoBundleInstall! +qall`
