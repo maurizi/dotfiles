87e7abed9bb54b70cc9938d8843381211f403239
Michael Maurizi <hollaburoo@gmail.com>
2014-08-03 22:25:26 -0400

    Chacking in more configs - git and zsh

--- scripts/common.sh
+++ scripts/common.sh
@@ -19,16 +19,6 @@ hub clone --quiet iam-TJ/git-submodule-move $DOTFILES/unix/bin/git-submodule-mov
 echo "Installing git-hooks"
 hub clone icefox/git-hooks $DOTFILES/unix/bin/git-hooks
 
-echo "Setting git aliases"
-git config --global alias.root '!pwd'
-git config --global alias.prune-all '!git remote | xargs -n 1 git remote prune'
-git config --global alias.tree 'log --graph --decorate --pretty=oneline --abbrev-commit'
-git config --global alias.ls 'log --pretty=format:"%C(yellow)%h%Cred%d\ %Creset%s%Cblue\ [%cn]" --decorate'
-git config --global alias.ll 'log --pretty=format:"%C(yellow)%h%Cred%d\ %Creset%s%Cblue\ [%cn]" --decorate --numstat'
-
-git config --global core.editor vim
-git config --global push.default current
-
 if [ ! -e ~/.vim/bundle/neobundle.vim ]
 then
     echo "Cloning NeoBundle"
