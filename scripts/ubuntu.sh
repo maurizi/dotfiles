af4c8ab302f40c7013fa5893720d8cbd37a9a2a2
Michael Maurizi <hollaburoo@gmail.com>
2014-08-05 16:20:47 -0400

    Add user level Vagrantfile for provisioning dotfiles

--- scripts/ubuntu.sh
+++ scripts/ubuntu.sh
@@ -4,6 +4,11 @@ sudo apt-get install stow
 
 sudo apt-get install tmux
 
+if [ `lsb_release -cs` == "precise" ];
+then
+    sudo add-apt-repository ppa:mizuno-as/silversearcher-ag
+    sudo apt-get update
+fi
 sudo apt-get install -qqy silversearcher-ag
 
 # For tern
@@ -15,8 +20,12 @@ then
     curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 fi
 
-echo "Installing hub"
 sudo apt-get install -qqy ruby
+echo "Installing hub"
+if [ `lsb_release -cs` == "precise" ];
+then
+    sudo apt-get install -qqy rubygems
+fi
 sudo gem install -qy hub >/dev/null
 
 source $(dirname $0)/common.sh
--- scripts/vim.cmd
+++ scripts/vim.cmd
@@ -9,6 +9,7 @@ set BASE_DIR=%DOTFILES%/unix
 
 call mklink "%HOME%/_vimrc" "%BASE_DIR%/.vimrc"
 call mklink /J "%HOME%/vimfiles" "%BASE_DIR%/.vim"
+call mklink "%HOME%/.vagrant.d/Vagrantfile" "%BASE_DIR%/.vagrant.d/Vagrantfile"
 
 IF NOT EXIST "%BASE_DIR%/vim/bundle" (
         call mkdir "%BASE_DIR%/vim/bundle"
