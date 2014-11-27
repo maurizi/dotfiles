188fd910964c98cca69fbcba99a916bb757f3233
Michael Maurizi <mmaurizi@azavea.com>
2014-11-27 15:37:41 -0500

    Use mklink to stow windows configs

--- scripts/babun.sh
+++ scripts/babun.sh
@@ -8,7 +8,7 @@ pact install tmux
 
 echo "Installing hub"
 pact install ruby
-gem install -qy hub >/dev/null
+gem install -q hub >/dev/null
 
 # For NeoComplete
 pact install lua
@@ -21,10 +21,28 @@ pip install ecdsa fabric
 # For ansible
 pip install pyyaml jinja2 ansible
 
-# Windows configs: copy because we can't symlink
-DOTFILES=$(git rev-parse --show-toplevel);
-cd $DOTFILES
+# Windows configs: a little manual because we can't stow
+# TODO: Migrate from stow to custom setup everywhere
 
-cp -rTv --remove-destination windows/ $USERPROFILE
+# Explicitly remove and then link vimfiles
+rm $USERPROFILE/vimfiles
+cmd /c mklink /J $USERPROFILE\\vimfiles $(cygpath --windows $DOTFILES/windows/vimfiles)
 
-source $(dirname $0)/common.sh
+# Use 'ag' to find appropriate files to link
+# For each file, make their base directory if necessary first
+cd $DOTFILES/windows
+FILES=$(ag -l --hidden '.*' . 2>/dev/null)
+for FILE in $FILES; do
+    UNIX_FILE=$(cygpath --unix $FILE)
+    TARGET_PATH=$(cygpath --unix $USERPROFILE)/$UNIX_FILE
+    BASEDIR=$(dirname $TARGET_PATH)
+    mkdir -p $BASEDIR
+    SYMLINK=$(cygpath -w $TARGET_PATH)
+    TARGET=$(cygpath -w $DOTFILES/windows/$UNIX_FILE)
+    # Weirdness: cygpath is adding an unreadable char to the end of my paths....
+    SYMLINK=${SYMLINK:0:${#SYMLINK}-1}
+    TARGET=${TARGET:0:${#TARGET}-1}
+    cmd /c mklink "$SYMLINK" "$TARGET"
+done
+
+source $DOTFILES/scripts/common.sh
