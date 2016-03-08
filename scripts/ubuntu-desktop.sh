9924c35ead995dd688e3346416665d14b77fb9ad
Michael Maurizi <mmaurizi@azavea.com>
2016-03-08 11:48:28 -0500

    Fix broken Chrome apt repository

--- scripts/ubuntu-desktop.sh
+++ scripts/ubuntu-desktop.sh
@@ -10,7 +10,7 @@ sudo apt-add-repository -y ppa:yubico/stable
 
 # Google Chrome
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google.list'
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google.list'
 
 sudo apt-get update
 
